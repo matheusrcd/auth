e8e76c8c0e575e4c80b7dea8f9c372bee99b4646
Matheus Ricardo Tomas <matheusrcdtms@hotmail.com>
2021-02-15 19:26:52 -0300

    connecting to  mongodb

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
+dotenv.config();
+
+
+//Connecting to DataBase
+mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true }, () => console.log('Connected to DataBase'));
+
+
+// Importing Routes
 const authRoute = require('./routes/auth');
 
 // Route Middlewares
